6a17a9d133d82093e7bfce81edded51aa570b6dd
Rahul Kumawat <rahul.kumawat@encoresky.com>
2024-09-06 16:29:49 +0530

    linting changes

--- gusto-pos-ts/src/app/(dahboard-group)/dashboard/page.tsx
+++ gusto-pos-ts/src/app/(dahboard-group)/dashboard/page.tsx
@@ -1,8 +1,14 @@
 "use client";
-import { Box, Paper, Stack, Typography, Grid, TextField, useMediaQuery } from "@mui/material";
+import { Box, Paper, Stack, Typography, useMediaQuery } from "@mui/material";
 import GSSelectInput from "@/components/widgets/inputs/GSSelect";
 import PageHeader from "@/components/widgets/headers/PageHeader";
-import { hours, productExpiryData, productStockData, stalesBreakDownReportData, statisticsData } from "@/mock/dashboard";
+import {
+  hours,
+  productExpiryData,
+  productStockData,
+  stalesBreakDownReportData,
+  statisticsData,
+} from "@/mock/dashboard";
 import { LineChart } from "@mui/x-charts";
 import { StatisticsCard } from "@/components/dashboard/StatisticsCard";
 import { useLocalization } from "@/context/LocalizationProvider";
@@ -10,47 +16,65 @@ import SalesReportBreakdown from "@/components/dashboard/SalesReportBreakdown";
 import { DashboardNote } from "@/components/dashboard/DashboardNote";
 import { ProductStockAlert } from "@/components/dashboard/ProductStock";
 import { ProductExpiryAlert } from "@/components/dashboard/ProductExpiry";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-// import Grid from "@mui/material/Unstable_Grid2/Grid2";
-
 
 export default function Home() {
-  const { translate } = useLocalization()
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const { translate } = useLocalization();
+  const { prefersDarkMode } = useMediaQuery("(prefers-color-scheme: dark)");
 
   return (
     <Box sx={{ flex: "1 1 auto", p: 3 }}>
-      <PageHeader title="Dashboard"/>
-      <Stack direction={"row"} sx={{ justifyContent: "space-between" }} spacing={2}>
+      <PageHeader title="Dashboard" />
+      <Stack
+        direction={"row"}
+        sx={{ justifyContent: "space-between" }}
+        spacing={2}
+      >
         {statisticsData.map((data, index) => (
-          <StatisticsCard key={index} title={data.title} value={data.value} isPositive={data.isPositive} />
+          <StatisticsCard
+            key={index}
+            title={data.title}
+            value={data.value}
+            isPositive={data.isPositive}
+          />
         ))}
       </Stack>
       <Paper sx={{ mt: 2, p: 2 }}>
         <Stack direction={"row"} justifyContent={"space-between"}>
-          <Typography >{translate("sales_breakdowns")}{" "}
-          </Typography>
-          <GSSelectInput options={["Today", "This Week", "This Month"]} value={"Today"} handleChange={() => { }} />
+          <Typography>{translate("sales_breakdowns")} </Typography>
+          <GSSelectInput
+            options={["Today", "This Week", "This Month"]}
+            value={"Today"}
+            handleChange={() => {}}
+          />
         </Stack>
         <LineChart
-          xAxis={[{ scaleType: 'point', data: hours }]}
+          xAxis={[{ scaleType: "point", data: hours }]}
           series={[
             {
-              data: [2, 3, 3.5, 4.5, 2.5, 5, 1, 4, 3, 8, 9, 10, 2, 3, 3.5, 4.5, 2.5, 5, 1, 4, 3, 8, 9, 10],
+              data: [
+                2, 3, 3.5, 4.5, 2.5, 5, 1, 4, 3, 8, 9, 10, 2, 3, 3.5, 4.5, 2.5,
+                5, 1, 4, 3, 8, 9, 10,
+              ],
             },
           ]}
           height={400}
         />
       </Paper>
 
-      <Stack direction={"row"} spacing={2} mt={2} sx={{
-        flexDirection: {
-          sm: 'row',  // Column direction on extra-small screens (mobile)
-          xs: 'column',     // Row direction on small screens (tablets) and up
-        },
-      }}>
-        <SalesReportBreakdown stalesBreakDownReportData={stalesBreakDownReportData} />
+      <Stack
+        direction={"row"}
+        spacing={2}
+        mt={2}
+        sx={{
+          flexDirection: {
+            sm: "row", // Column direction on extra-small screens (mobile)
+            xs: "column", // Row direction on small screens (tablets) and up
+          },
+        }}
+      >
+        <SalesReportBreakdown
+          stalesBreakDownReportData={stalesBreakDownReportData}
+        />
         <Stack flex={1} sx={{ height: "fit-content" }}>
           <DashboardNote />
           <ProductStockAlert productStockData={productStockData} />
