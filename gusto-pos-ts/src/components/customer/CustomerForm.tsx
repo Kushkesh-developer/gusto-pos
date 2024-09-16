056fba06947fe93f68757ab198fe57a9851984b7
“kushkesh” <kushkesh.bhadauriya@encoresky.com>
2024-09-16 16:59:51 +0530

    GSDaySelector

--- gusto-pos-ts/src/components/customer/CustomerForm.tsx
+++ gusto-pos-ts/src/components/customer/CustomerForm.tsx
@@ -34,28 +34,30 @@ import { TranslateFn } from "@/types/localization-types";
     selectedDays: { value: string }[]; // Use array of objects for useFieldArray
   }
 
-const generateZodSchema = (translate: any) => {
-  return z.object({
-    gender: z.string().min(1, translate("gender_required")),
-    name: z.string().min(1, translate("customer_name_required")),
-    phoneNumber: z.string().min(1, translate("phone_number_required")),
-    email: z.string().email(translate("invalid_email")),
-    customerGroup: z.string().min(1, translate("customer_group_required")),
-    dateOfBirth: z.date().max(new Date(), translate("date_of_birth_past")),
-    maritalStatus: z.string().min(1, translate("marital_status_required")),
-    nationality: z.string().min(1, translate("nationality_required")),
-    facebook: z.string().optional(),
-    linkedIn: z.string().optional(),
-    twitter: z.string().optional(),
-    address: z.string().min(1, translate("address_required")),
-    numberOfPurchases: z.string().min(1, translate("number_of_purchases_required")),
-    lowestSpend: z.string().min(1, translate("lowest_spend_required")),
-    highestSpend: z.string().min(1, translate("highest_spend_required")),
-    avgSpend: z.string().min(1, translate("average_spend_required")),
-    note: z.string().optional(),
-    selectedDays: z.array(z.string()).min(1, translate("day_required")) // Add validation for selected days
-  });
-};
+  const generateZodSchema = (translate: any) => {
+    return z.object({
+      gender: z.string().min(1, translate("gender_required")),
+      name: z.string().min(1, translate("customer_name_required")),
+      phoneNumber: z.string().min(1, translate("phone_number_required")),
+      email: z.string().email(translate("invalid_email")),
+      customerGroup: z.string().min(1, translate("customer_group_required")),
+      dateOfBirth: z.date().max(new Date(), translate("date_of_birth_past")),
+      maritalStatus: z.string().min(1, translate("marital_status_required")),
+      nationality: z.string().min(1, translate("nationality_required")),
+      facebook: z.string().optional(),
+      linkedIn: z.string().optional(),
+      twitter: z.string().optional(),
+      address: z.string().min(1, translate("address_required")),
+      numberOfPurchases: z.string().min(1, translate("number_of_purchases_required")),
+      lowestSpend: z.string().min(1, translate("lowest_spend_required")),
+      highestSpend: z.string().min(1, translate("highest_spend_required")),
+      avgSpend: z.string().min(1, translate("average_spend_required")),
+      note: z.string().optional(),
+      selectedDays: z
+        .array(z.object({ value: z.string() }))
+        .min(1, translate("day_required")), // Array of objects with day values
+    });
+  };
 
   const CustomerForm = () => {
     const { translate } = useLocalization();
@@ -107,10 +109,9 @@ const generateZodSchema = (translate: any) => {
       }
     };
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    const formDataWithDays = { ...data, selectedDays };
-    console.log(formDataWithDays);
-  };
+    const onSubmit: SubmitHandler<FormData> = (data) => {
+      console.log(data);
+    };
 
     return (
       <Box sx={{ maxWidth: "1140px" }}>
@@ -260,17 +261,17 @@ const generateZodSchema = (translate: any) => {
             </FormLayout>
           </Box>
 
-        <Box display="flex" justifyContent="flex-end" mt={3}>
-          <CustomButton variant="outlined" type="button" sx={{ mr: 2 }}>
-            {translate("cancel")}
-          </CustomButton>
-          <CustomButton variant="contained" type="submit">
-            {translate("save")}
-          </CustomButton>
-        </Box>
-      </form>
-    </Box>
-  );
-};
+          <Box display="flex" justifyContent="flex-end" mt={3}>
+            <CustomButton variant="outlined" type="button" sx={{ mr: 2 }}>
+              {translate("cancel")}
+            </CustomButton>
+            <CustomButton variant="contained" type="submit">
+              {translate("save")}
+            </CustomButton>
+          </Box>
+        </form>
+      </Box>
+    );
+  };
 
-export default CustomerForm;
+  export default CustomerForm;
