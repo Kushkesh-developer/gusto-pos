727d7d535646f94b3b7b0e95a0851d6c5e704f0e
Rahul Kumawat <rahul.kumawat@encoresky.com>
2024-09-17 19:43:00 +0530

    minor glitch fix

--- gusto-pos-ts/src/app/(dahboard-group)/suppliers/view-suppliers/page.tsx
+++ gusto-pos-ts/src/app/(dahboard-group)/suppliers/view-suppliers/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {  Typography, Divider, useTheme,Box} from "@mui/material";
+import { Typography, Divider, useTheme, Box } from "@mui/material";
 import GSTable from "@/components/widgets/table/GSTable";
 import GSTableControls from "@/components/widgets/table/GSTableControls";
 // Mock data
@@ -42,50 +42,48 @@ const Page = () => {
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = filteredUsers.slice(indexOfFirstItem, indexOfLastItem);
   const totalPages = Math.ceil(filteredUsers.length / itemsPerPage);
-   // Centralized column configuration
-   const columnNames = [
+  // Centralized column configuration
+  const columnNames = [
     { label: "Company Name", key: "Company Name", visible: true },
     { label: "Contact Person", key: "Contact Person", visible: true },
     { label: "Mobile", key: "Mobile", visible: true },
     { label: "Office", key: "Office", visible: true },
     { label: "Email", key: "Email", visible: true },
-    { label:"Postal Code",key:"Postal Code",visible:true},
-    { label: "Action", key: "action", visible: true ,isAction:true},
+    { label: "Postal Code", key: "Postal Code", visible: true },
+    { label: "Action", key: "action", visible: true, isAction: true },
   ];
-  const [columns, setColumns] = useState(columnNames)
-
-
+  const [columns, setColumns] = useState(columnNames);
 
   // Filter users based on search query
   useEffect(() => {
     const filteredRows = response.filter((user) => {
-      const users = `${user["Company Name"]} ${user["Contact Person"]} ${user.Mobile} ${user.Office} ${user.Email}`.toLowerCase();
+      const users =
+        `${user["Company Name"]} ${user["Contact Person"]} ${user.Mobile} ${user.Office} ${user.Email}`.toLowerCase();
       const sanitizedSearch = searchQuery.toLowerCase().trim();
       return users.includes(sanitizedSearch);
     });
     setFilteredUsers(filteredRows);
   }, [searchQuery, response]);
 
- 
   return (
-    <Box style={{padding:"24px"}} >
+    <Box style={{ padding: "24px" }}>
       <Typography variant="h4" gutterBottom color={theme.palette.primary.main}>
         View Supplier
       </Typography>
       <Divider />
-      <Box style={{marginTop:"15px"}}>
-      <GSTableControls
-        setSearchQuery={setSearchQuery}
-        setColumnsVisibility={(newColumns) => setColumns(newColumns)}
-        columns={columns}
-        TableTitle="Add new supplier"
-        showPrint
-        showExcel
-        showPdf
-        showFilter
-        href="/staff/add-staff"
-      />
-      </B>
+      <Box style={{ marginTop: "15px" }}>
+        <GSTableControls
+          setSearchQuery={setSearchQuery}
+          setColumnsVisibility={(newColumns) => setColumns(newColumns)}
+          columns={columns}
+          TableTitle="Add new supplier"
+          showPrint
+          showExcel
+          showPdf
+          showFilter
+          href="/staff/add-staff"
+        />
+      </Box>
       <GSTable
         columns={columns}
         filteredUsers={filteredUsers}
@@ -93,11 +91,10 @@ const Page = () => {
         currentPage={currentPage}
         totalPages={totalPages}
         handlePageChange={(e, page) => setCurrentPage(page)}
-        keyMapping={Object.fromEntries(columnNames.map((col) => [col.label, col.key]))}
-
-       
+        keyMapping={Object.fromEntries(
+          columnNames.map((col) => [col.label, col.key]),
+        )}
       />
-
     </Box>
   );
 };
