5d4971bed0a4a8da80da087a615ce7af36f91a1e
Rahul Kumawat <rahul.kumawat@encoresky.com>
2024-09-19 15:04:43 +0530

    Stock page additional change

--- gusto-pos-ts/src/components/stock-manager/StockTable.tsx
+++ gusto-pos-ts/src/components/stock-manager/StockTable.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function StockTable() {
-  return <></>;
+import GSTable, {
+  ColumnType,
+  GSTableData,
+} from "@/components/widgets/table/GSTable";
+
+type StockTableProps = {
+  columns: ColumnType[];
+  filteredUsers: GSTableData[];
+  currentItems: GSTableData[];
+  currentPage: number;
+};
+
+export default function StockTable(props: StockTableProps) {
+  const { columns, filteredUsers } = props;
+  // Pagination
+  const [currentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  const currentItems = filteredUsers.slice(indexOfFirstItem, indexOfLastItem);
+
+  return (
+    <GSTable
+      columns={columns}
+      filteredUsers={filteredUsers}
+      currentItems={currentItems} // Ensure this is passed
+      currentPage={currentPage}
+      totalPages={10}
+      sx={{
+        mt: 2,
+        minHeight: { xs: "300px", md: "400px", xl: "80%" },
+      }}
+    />
+  );
 }
--- gusto-pos-ts/src/components/widgets/table/GSTable.tsx
+++ gusto-pos-ts/src/components/widgets/table/GSTable.tsx
@@ -10,11 +10,10 @@ import {
   TableContainer,
   Paper,
 } from "@mui/material";
-import Link from "next/link";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import { alpha, useTheme } from '@mui/material/styles';
+import { alpha, SxProps, useTheme } from '@mui/material/styles';
 import PaginationComponent from "./Pagination";
 
 interface ColumnType {
@@ -25,6 +24,8 @@ interface ColumnType {
   isVisibility?: boolean; 
 }
 
+export type GSTableData = Record<string, unknown>[];
+
 interface TableProps {
   columns: ColumnType[];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -33,9 +34,12 @@ interface TableProps {
   currentItems: any[];
   currentPage: number;
   totalPages: number;
-  // eslint-disable-next-line no-unused-vars
-  handlePageChange: (event: React.ChangeEvent<unknown>, page: number) => void;
-  keyMapping: { [key: string]: string };
+  handlePageChange?: (
+    _event: React.ChangeEvent<unknown>,
+    _page: number,
+  ) => void;
+  keyMapping?: { [key: string]: string };
+  sx?: SxProps;
 }
 
 const GSTable = ({
@@ -44,12 +48,13 @@ const GSTable = ({
   currentItems,
   currentPage,
   totalPages,
-  handlePageChange,
+  handlePageChange = () => {},
+  sx = {},
 }: TableProps) => {
   const theme = useTheme();
 
   return (
-    <TableContainer component={Paper} style={{ paddingBottom: "10px" }}>
+    <TableContainer component={Paper} sx={{ pb: 2, ...sx }}>
       <Table>
         <TableHead
           style={{
@@ -85,20 +90,16 @@ const GSTable = ({
                   return (
                     <TableCell key={column.key}>
                       {column.isAction ? (
-                        <Box sx={{ display: 'flex', gap: 0 }}>
-                          {/* Conditional rendering of Edit and Delete icons */}
-                          <IconButton component={Link} href={`/attendance/${value.id}`}>
-                            <EditIcon style={{ color: theme.palette.primary.main }} />
+                        <Box sx={{ display: "flex", gap: 0 }}>
+                          <IconButton onClick={() => console.log("edit")}>
+                            <EditIcon
+                              style={{ color: theme.palette.primary.main }}
+                            />
                           </IconButton>
-                          <IconButton component={Link} href={`/attendance/${value.id}`}>
-                            <DeleteIcon style={{ color: theme.palette.primary.main }} />
-                          </IconButton>
-                        </Box>
-                      ) : column.isVisibility ? (
-                        <Box sx={{ display: 'flex', gap: 0 }}>
-                          {/* Conditional rendering of Visibility icon */}
-                          <IconButton component={Link} href={`/attendance/${value.id}`}>
-                            <VisibilityIcon style={{ color: theme.palette.primary.main }} />
+                          <IconButton onClick={() => console.log("Delete")}>
+                            <DeleteIcon
+                              style={{ color: theme.palette.primary.main }}
+                            />
                           </IconButton>
                         </Box>
                       ) : (
