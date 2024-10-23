a3979efcdf8216c6b42fb64149d171a9e42929e0
kushekesh bhadauriya <kushekesh.bhadauriya@encoresky.com>
2024-10-23 16:47:10 +0530

    minor spell fix.

--- gusto-pos-ts/src/app/(dahboard-group)/modifier/view-modifier-group/page.tsx
+++ gusto-pos-ts/src/app/(dahboard-group)/modifier/view-modifier-group/page.tsx
@@ -7,7 +7,7 @@ import { theme } from "@/theme/theme";
 import ModifierGroupDrawer from "@/components/modifier/ModifierGroupDrawer";
 import { ColumnType } from "@/types/table-types";
 import { useLocalization } from "@/context/LocalizationProvider";
-import { modifierGroupmock } from "@/mock/modifier";
+import { modifierGroupMock } from "@/mock/modifier";
 const columnNames: ColumnType[] = [
   { label: "Group", key: "group", visible: true },
   {
@@ -31,8 +31,8 @@ const columnNames: ColumnType[] = [
 ];
 const Page = () => {
   const { translate } = useLocalization();
-  const [response] = useState(modifierGroupmock);
-  const [filteredUsers, setFilteredUsers] = useState(modifierGroupmock);
+  const [response] = useState(modifierGroupMock);
+  const [filteredUsers, setFilteredUsers] = useState(modifierGroupMock);
   const [showUserDrawer, setShowUserDrawer] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
